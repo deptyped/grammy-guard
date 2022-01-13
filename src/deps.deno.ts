53adf98c6ccb0cb7014b2dd4e308f3900e029e5a
Satont <satontworldwide@gmail.com>
2022-01-13 21:13:50 +0300

    Fix deno version

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,1 +1,1 @@
-export { Bot, Context, NextFunction, MiddlewareFn } from 'https://deno.land/x/grammy@1.6.1/mod.ts'
+export { Bot, Context, NextFunction, MiddlewareFn } from 'https://deno.land/x/grammy@v1.6.1/mod.ts'
